104b05a9e3d1601464dd0d27f8426636f69de489
GF <149839341+gorillaframeai@users.noreply.github.com>
2025-03-26 10:37:07 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,12 @@
 from .gfrbmg2 import GFrbmg2
+from .gfrbmg2plus import GFrbmg2Plus
 
 NODE_CLASS_MAPPINGS = {
-    "GFrbmg2": GFrbmg2
+    "GFrbmg2": GFrbmg2,
+    "GFrbmg2Plus": GFrbmg2Plus
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "GFrbmg2": "🐵 GF Remove Background 2.0"
+    "GFrbmg2": "🐵 GF Remove Background 2.0",
+    "GFrbmg2Plus": "🐵 GF Remove Background (GFrbmg2Plus)"
 }
